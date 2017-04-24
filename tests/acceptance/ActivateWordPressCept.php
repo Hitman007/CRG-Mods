82542d5642b8e7324636425cb6b92b0c28238cd3
jim <jiminac@aol.com>
2017-04-23 17:52:01 -0700

    HI

---
+++ tests/acceptance/ActivateWordPressCept.php
@@ -0,0 +1,18 @@
+<?php
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Install WordPress');
+$I->anOnPage('/wp-admin/setup-config.php');
+$I->click("Let's Go!");
+$I->fillField("dbname", "wordpress");
+$I->fillField("uname", "wordpressuser");
+$I->fillField("pwd", "password");
+$I->fillField("dbhost", "localhost");
+$I->fillField("prefix", "wp_");
+$I->click('Submit');
+$I->click('Run the install');
+$I->fillField("weblog_title", "Custom Ray Guns Remote");
+$I->fillField("user_name", "admin");
+$I->fillField("pass1-text", "password");
+$I->fillField("admin_email", "admin@thissite.com");
+$I->click('Install WordPress');
