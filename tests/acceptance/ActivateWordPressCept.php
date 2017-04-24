eb7a73c7d34cfb40dcd7f751a3423f982d2c44d3
Hitman007 <support@customrayguns.com>
2017-04-23 18:26:33 -0700

    Update ActivateWordPressCept.php

--- tests/acceptance/ActivateWordPressCept.php
+++ tests/acceptance/ActivateWordPressCept.php
@@ -13,6 +13,6 @@ $I->click('Submit');
 $I->click('Run the install');
 $I->fillField("weblog_title", "Custom Ray Guns Remote");
 $I->fillField("user_name", "admin");
-$I->fillField("pass1-text", "password");
+$I->fillField("#pass1-text", "password");
 $I->fillField("admin_email", "admin@thissite.com");
 $I->click('Submit');
